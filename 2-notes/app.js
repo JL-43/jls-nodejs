924e6ff1b9f0df16e4dde4d7c2f6735ae4d15cee
JL Reyes <joseluisreyes43@gmail.com>
2020-06-15 19:43:18 +0800

    required my first local file

--- 2-notes/app.js
+++ 2-notes/app.js
@@ -1,7 +1,17 @@
 const fs = require('fs')
 
-//from the video
-fs.writeFileSync('notes.txt', 'My name is JL')
+require('./utils.js');
 
-//from the challenge
-fs.appendFileSync('notes.txt', ' and I like to code')
+//1
+
+// //from the video
+// fs.writeFileSync('notes.txt', 'My name is JL')
+
+// //from the challenge
+// fs.appendFileSync('notes.txt', ' and I like to code')
+
+//2
+
+const name = 'JL'
+
+console.log(name);
