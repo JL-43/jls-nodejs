14673e232a3f580cbfec7abe1298a3d9e72db3b9
JL Reyes <joseluisreyes43@gmail.com>
2020-06-17 13:31:22 +0800

    finished the challenge with overwriting data from andrew with my own data

--- playground/1-json.js
+++ playground/1-json.js
@@ -1,21 +1,31 @@
 const fs = require('fs');
 
-const book = {
-    title: 'A Man Called Ove',
-    author: 'Fredrik Backman'
-}
+// const book = {
+//     title: 'A Man Called Ove',
+//     author: 'Fredrik Backman'
+// }
 
-//object -> json string
-const bookJSON = JSON.stringify(book);
-console.log(bookJSON)
+// //object -> json string
+// const bookJSON = JSON.stringify(book);
+// console.log(bookJSON)
 
-//json string -> object
-const bookParsed = JSON.parse(bookJSON);
-console.log(bookParsed)
+// //json string -> object
+// const bookParsed = JSON.parse(bookJSON);
+// console.log(bookParsed)
 
-// fs.writeFileSync('1-json.json', bookJSON);
+// // fs.writeFileSync('1-json.json', bookJSON);
 
-const dataBuffer = fs.readFileSync('1-json.json');
-const dataJSON = dataBuffer.toString();
-const data = JSON.parse(dataJSON);
-console.log(data.title)
+// const dataBuffer = fs.readFileSync('1-json.json');
+// const dataJSON = dataBuffer.toString();
+// const data = JSON.parse(dataJSON);
+// console.log(data.title)
+
+//challenge
+
+const personBuffer = fs.readFileSync('1-json.json'); //get data (not human friendly)
+const personJSON = personBuffer.toString(); //convert data into json string
+const person = JSON.parse(personJSON); //convert json string into object
+person.name = 'JL'
+person.age = 23
+const personJSONNew = JSON.stringify(person);
+fs.writeFileSync('1-jsonnew.json', personJSONNew);
