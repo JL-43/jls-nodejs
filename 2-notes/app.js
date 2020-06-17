cce2cd314399b1f11f47288c5f0be4e09af4607b
JL Reyes <joseluisreyes43@gmail.com>
2020-06-17 11:12:46 +0800

    set up my command builders for the add command

--- 2-notes/app.js
+++ 2-notes/app.js
@@ -67,8 +67,21 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: () => {
-        console.log('Adding a new note');
+    builder:{
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        console.log('Title: ' + argv.title);
+        console.log('Body: ' + argv.body)
     }
 })
 
@@ -102,5 +115,6 @@ yargs.command({
 })
 
 // add, remove, list, read
-console.log(yargs.argv)
+yargs.parse()
+// console.log(yargs.argv)
 
