0cdad033a680f4891f4af74dbf73ff334a75c449
JL Reyes <joseluisreyes43@gmail.com>
2020-06-18 15:02:39 +0800

    added geo functionality using a geocode api. Created a simple geo request and fixed apikeys setup

---
+++ 3-weather/apikeys.js
@@ -0,0 +1,7 @@
+const weatherapikey = 'b4913174864c088c9669c079524e904c';
+const geoapikey = 'pk.eyJ1IjoiamxtcmV5ZXM0MyIsImEiOiJja2JrZXR3a2YweWI2MnJxaWdoMDFhc3cyIn0.LULvQNXPcIPUjt6x6AujoQ';
+
+module.exports = {
+    weatherapikey,
+    geoapikey
+}
--- 3-weather/app.js
+++ 3-weather/app.js
@@ -1,10 +1,16 @@
 const request = require('request');
-const apikey = require('./apikey.js')
-const query = '14.5995,120.9842'
+const apikeys = require('./apikeys.js')
 
-const url = `http://api.weatherstack.com/current?access_key=${apikey}&query=${query}`;
+const geo_query = `https://api.mapbox.com/geocoding/v5/mapbox.places/Manila.json?access_token=${apikeys.geoapikey}`
+request({url: geo_query, json: true}, (error, response) => {
+    const longtitude = response.body.features[0].geometry.coordinates[0];
+    const latitude = response.body.features[0].geometry.coordinates[1];
+    console.log(`Latitude: ${latitude} & Longtitude: ${longtitude}`)
+});
 
-request({url: url, json: true}, (error, response) => {
-    // console.log(response.body.current)
-    console.log(`It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C though`)
+const location = `${latitude},${longtitude}`
+const weather_query = `http://api.weatherstack.com/current?access_key=${apikeys.weatherapikey}&query=${location}`;
+
+request({url: weather_query, json: true}, (error, response) => {
+    console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C though`)
 });
