449447039b355c0564d1bba646e0e95818180d45
JL Reyes <joseluisreyes43@gmail.com>
2020-06-18 14:27:15 +0800

    accessed specific data from the json object and logged it for the user

--- 3-weather/app.js
+++ 3-weather/app.js
@@ -4,7 +4,7 @@ const query = '14.5995,120.9842'
 
 const url = `http://api.weatherstack.com/current?access_key=${apikey}&query=${query}`;
 
-request({url: url}, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current);
+request({url: url, json: true}, (error, response) => {
+    // console.log(response.body.current)
+    console.log(`It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C though`)
 });
