e9b5b63e05d8c73fe065b2d1fd85d1a92754fd1b
JL Reyes <joseluisreyes43@gmail.com>
2020-06-19 12:15:10 +0800

    turned geocode into a function with callback functionality. Created the utils folder with apikeys & geocode function inside

--- 3-weather/app.js
+++ 3-weather/app.js
@@ -1,29 +1,20 @@
 const request = require('request');
-const apikeys = require('./apikeys.js')
-// const location = `${latitude},${longtitude}`;
-const location = '14.58333,121';
-const weather_query = `http://api.weatherstack.com/current?access_key=${apikeys.weatherapikey}&query=${location}`;
+const geocode = require('./utils/geocode.js');
+// const location = `${latitude},${longitude}`;
+// const location = '14.58333,121';
+// const weather_query = `http://api.weatherstack.com/current?access_key=${apikeys.weatherapikey}&query=${location}`;
 
-request({url: weather_query, json: true}, (error, response) => {
-    if(error){
-        console.log('Unable to connect to weather service!');
-    }else if(response.body.error){
-        console.log('Unable to find location');
-    }else{
-        console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C though`);
-    }
+// request({url: weather_query, json: true}, (error, response) => {
+//     if(error){
+//         console.log('Unable to connect to weather service!');
+//     }else if(response.body.error){
+//         console.log('Unable to find location');
+//     }else{
+//         console.log(`${response.body.current.weather_descriptions[0]}. It is currently ${response.body.current.temperature}C outside. It feels like ${response.body.current.feelslike}C though`);
+//     }
+// });
+
+geocode('Manila', (error, data) => {
+    console.log('Error', error);
+    console.log('Data', data)
 });
-
-const geo_query = `https://api.mapbox.com/geocoding/v5/mapbox.places/Manila.json?access_token=${apikeys.geoapikey}`
-request({url: geo_query, json: true}, (error, response) => {
-    if(error){
-        console.log('Unable to connect to Geocode Service');
-    }else if(response.body.features.length == 0){
-        console.log('Unable to find that location')
-    }else{
-        const longtitude = response.body.features[0].geometry.coordinates[0];
-        const latitude = response.body.features[0].geometry.coordinates[1];
-        console.log(`Latitude: ${latitude} & Longtitude: ${longtitude}`)
-    }
-});
-
