ad4d759ca1b8148b7fc3aba46b9ef5fab0c8cdc1
JL Reyes <joseluisreyes43@gmail.com>
2020-06-19 14:48:07 +0800

    refactored code a bit

--- 3-weather/app.js
+++ 3-weather/app.js
@@ -1,20 +1,20 @@
-const request = require('request');
 const geocode = require('./utils/geocode.js');
 const weather = require('./utils/weather.js');
 
-geocode('Manila', (error, data) => {
-    if(data){
-        weather(data.latitude, data.longitude, data.location, (error, data) => {
-            if(data){
-                console.log(`Welcome to ${data.location}. 
-                    Right now, it's ${data.description} 
-                    at ${data.temperature}, 
-                    but it feels like ${data.feelslike}`);
-            }else{
-                console.log(error);
-            }
-        })
-    }else{
-        console.log(error);
+geocode('Makati', (error, data) => {
+
+    if(error){
+        return console.log(error)
     }
+
+    weather(data.latitude, data.longitude, data.location, (error, data) => {
+        if(error){
+            return console.log(error);
+        }
+        console.log(`Welcome to ${data.location}. 
+            Right now, it's ${data.description} 
+            at ${data.temperature}, 
+            but it feels like ${data.feelslike}`);
+    })
+
 });
