6b55d63b66a9138018ab3f06dfdc9d168206bd50
JL Reyes <joseluisreyes43@gmail.com>
2020-06-22 13:06:50 +0800

    converted the about and the help routes into their own html pages

--- 4-webserver/src/app.js
+++ 4-webserver/src/app.js
@@ -5,16 +5,6 @@ const app = express();
 const publicDirectoryPath = path.join(__dirname, '../public'); 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/help', (req, res) => {
-    res.send([{
-        name: 'JL',
-        age: 23
-    }]);
-});
-
-app.get('/about', (req, res) => {
-    res.send('<h1>This is the about page</h1>');
-});
 
 app.get('/weather', (req, res) => {
     res.send([{
