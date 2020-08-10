5c48d73e95d4471b7bdbbfe6eef6c42016e5c23f
JL Reyes <joseluisreyes43@gmail.com>
2020-08-10 15:07:32 +0800

    cleared some comments

--- 4-webserver/src/app.js
+++ 4-webserver/src/app.js
@@ -68,14 +68,6 @@ app.get('/weather', (req, res) => {
             });
         })
     });
-
-    // geocode(req.query.address, (error, {latitude, longitude, location})) => {
-    //     res.send([{
-    //         forecast: 'It is raining',
-    //         location: req.query.address
-    //     }]);
-    // }
-    
 });
 
 app.get('/products', (req, res) => {
